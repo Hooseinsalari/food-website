deeda78b993e8bbb21751d720c28bae73d878a9c
Hooseinsalari <hossein.salari002@gmail.com>
2022-06-21 21:17:15 +0430

    add navbar and home

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector('.header');
+const navbar = document.querySelector('.nav');
+
+window.onscroll = function () { 
+    if (window.scrollY >= 100 ) {
+        navbar.classList.add("nav__scroll");
+        // navbar.classList.remove("nav-transparent");
+    } 
+    else {
+        // navbar.classList.add("nav-transparent");
+        navbar.classList.remove("nav__scroll");
+    }
+};
