c75fdc49b89e86758c87a5367bdac97ab8062824
Hooseinsalari <hossein.salari002@gmail.com>
2022-06-22 19:19:03 +0430

    add expert section

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.header');
 const navbar = document.querySelector('.nav');
 
 window.onscroll = function () { 
-    if (window.scrollY >= 100 ) {
+    if (window.scrollY > 100 ) {
         navbar.classList.add("nav__scroll");
         // navbar.classList.remove("nav-transparent");
     } 
