be34bf687a2d9a11e293dd1266f9c5ce3c3d0c57
Hooseinsalari <hossein.salari002@gmail.com>
2022-06-23 00:36:39 +0430

    add responsive

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const header = document.querySelector('.header');
-const navbar = document.querySelector('.nav');
+const header = document.querySelector(".header");
+const navbar = document.querySelector(".nav");
+const closeBtn = document.querySelector(".nav__close");
+const showMenuBtn = document.querySelector(".nav__toggle");
+const navMenu = document.querySelector(".nav__menu");
+const navItmes = document.querySelector(".nav__item");
 
-window.onscroll = function () { 
-    if (window.scrollY > 100 ) {
-        navbar.classList.add("nav__scroll");
-        // navbar.classList.remove("nav-transparent");
-    } 
-    else {
-        // navbar.classList.add("nav-transparent");
-        navbar.classList.remove("nav__scroll");
-    }
+window.onscroll = function () {
+  if (window.scrollY > 100) {
+    navbar.classList.add("nav__scroll");
+    // navbar.classList.remove("nav-transparent");
+  } else {
+    // navbar.classList.add("nav-transparent");
+    navbar.classList.remove("nav__scroll");
+  }
 };
+
+showMenuBtn.addEventListener("click", () => {
+  navMenu.style.transform = "translateX(0)";
+});
+
+closeBtn.addEventListener("click", () => {
+  navMenu.style.transform = "translateX(100%)";
+});
